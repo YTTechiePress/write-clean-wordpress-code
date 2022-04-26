77c664a7f0e8b5f9790af3daf6e9b2012b0e1724
Laurence Bahiirwa <laurence@mediauganda.com>
2022-04-26 21:45:12 +0300

    Dirty code

--- query-apis.php
+++ query-apis.php
@@ -8,51 +8,66 @@
  * version: 0.1.0
  * License: GPL2 or later.
  * text-domain: query-apis
- * 
- * @package query_apis
  */
 
 // If this file is access directly, abort!!!
-defined( 'ABSPATH' ) or die( 'Unauthorized Access' );
+defined( 'ABSPATH'
 
-/**
- * Gets data from API and var_dumps it.
- *
- * @return void
- */
+) or die( 'Unauthorized Access'
+
+);
+
 function techiepress_get_send_data() {
 
-	$url = 'https://jsonplaceholder.typicode.com/users';
+    $url = 'https://jsonplaceholder.typicode.com/users';$arguments = 
+	array('method' => 'GET');
+	$response = wp_remote_get( 
+		
+		
+		$url, $arguments 
 	
-	$arguments = array(
-		'method' => 'GET',
 	);
 
-	$response = wp_remote_get( $url, $arguments );
+	if (is_wp_error($response)){
+		$error_message = 
+		
 
-	if ( is_wp_error( $response ) ) {
-		$error_message = $response->get_error_message();
-		return "Something went wrong: $error_message";
-	} else {
-		echo '<pre>';
-		var_dump( wp_remote_retrieve_body( $response ) );
-		echo '</pre>';
-	}
-}
+		
+		$response->get_error_message();return
+		
+		
+		"Something went wrong: $error_message"; } else {
+		echo '<pre>'; var_dump( 
+			
+			wp_remote_retrieve_body( 
+			
+			
+			$response ) ); echo '</pre>';
+	}}	
 
 /**
  * Register a custom menu page to view the information queried.
  */
-function techiepress_register_my_custom_menu_page() {
+function 
+
+techiepress_register_my_custom_menu_page() {
 	add_menu_page(
-		__( 'Query API Test Settings', 'query-apis' ),
+		__( 
+			
+			
+			'Query API Test Settings', 'query-apis' ),
 		'Query API Test',
 		'manage_options',
-		'api-test.php',
-		'techiepress_get_send_data',
-		'dashicons-testimonial',
+
+
+		'api-test.php','techiepress_get_send_data','dashicons-testimonial',
 		16
 	);
 }
 
-add_action( 'admin_menu', 'techiepress_register_my_custom_menu_page' );
+add_action( 
+	'admin_menu', 'techiepress_register_my_custom_menu_page' 
+
+
+
+);
